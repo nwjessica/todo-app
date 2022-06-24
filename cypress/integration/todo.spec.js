eff602e021a2c52427132f429ba8df26fce0a38f
nwjessica <72961639+nwjessica@users.noreply.github.com>
2022-06-25 01:40:34 +0700

    Update todo.spec.js

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,42 +1,3 @@
-/// <reference types="Cypress" />
-
-// describe('TODO App > ', () => {
-//   beforeEach(() => {
-//     cy.visit('/')
-//   })
-
-//   it('OK, there are no TODOs >', () => {
-//     cy.get('#todo-body').children().should('have.length', 0);
-//     cy.get('#done-body').children().should('have.length', 0);
-//   });
-
-//   it('OK, create a TODO >', () => {
-//     cy.createTODO("Mop the floor");
-//     cy.get('#todo-body').children().should('have.length', 1);
-//     cy.get('#done-body').children().should('have.length', 0);
-//   });
-
-//   it('OK, create a TODO & mark it done >', () => {
-//     cy.createTODO("Hoover the floor");
-//     cy.get('button[cy-data="todo-0"]').click();
-//     cy.get('#todo-body').children().should('have.length', 0);
-//     cy.get('#done-body').children().should('have.length', 1);
-//   });
-
-//   it('OK, create 5 TODOs & mark 2 done >', () => {
-//     cy.createTODO("Mop the floor");
-//     cy.createTODO("Mop the floor");
-//     cy.createTODO("Mop the floor");
-//     cy.createTODO("Mop the floor");
-//     cy.createTODO("Mop the floor");
-//     cy.get('button[cy-data="todo-0"]').click();
-//     cy.get('button[cy-data="todo-3"]').click();
-//     cy.get('#todo-body').children().should('have.length', 3);
-//     cy.get('#done-body').children().should('have.length', 2);
-//   });
-// })
-
-//Punya Icha
 var count=0
 it('first test, no todos', ()=>{
   cy.visit('/')
@@ -47,12 +8,13 @@ it('first test, no todos', ()=>{
 })
 
 it('create 5 TODOs and mark 2 done',() =>{
-    
-    cy.createTODO('Interview 2 jam lagi')
-    cy.createTODO('Secukupnya')
-    cy.createTODO('Hindia')
-    cy.createTODO('Family Guy')
-    cy.createTODO('tulung Gusti')
+   cy.visit('/')
+  
+    cy.createTODO('Mop the floor')
+    cy.createTODO('Read book')
+    cy.createTODO('Hear Music')
+    cy.createTODO('Pray to God')
+    cy.createTODO('Sleep')
 
     
     cy.markdone(2)
